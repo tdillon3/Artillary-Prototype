1a931c96fb6a11d386c9e7cb5534be63b5a199cf
Tanner Dillon <tanner.dillon969@gmail.com>
2023-07-15 16:16:58 -0600

    Updated comments and cleaned up some code

--- bullet.h
+++ bullet.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <cassert>
 #include <iostream>
 #include <list>
@@ -7,61 +8,74 @@
 #include <deque>
 #include "physics.h"
 
-
-/// <summary>
-/// Test class to test the updating of the bullet
-/// </summary>
+// Class to manage bullet objects
 class Bullet
 {
 public:
+    // Store the X and Y components of the bullet's position
+    std::deque<double> listX;
+    std::deque<double> listY;
 
-	std::deque<double> listX;
-	std::deque<double> listY;
-	double componentX;
-	double componentY;
-	double x = 0.0;
-	double y = 0.0;
+    double componentX;  // The x-component of the bullet's movement
+    double componentY;  // The y-component of the bullet's movement
+    double x = 0.0;     // Current x position
+    double y = 0.0;     // Current y position
 
-	void newBullet(Position* howitzer)
-	{
-		listX.clear();
-		listY.clear();
-		for (int count = 1; count <= 19; count++)
-		{
-			listX.push_front(0);
-			listY.push_front(0);
-		}
-		x = howitzer->getMetersX();
-		y = howitzer->getMetersY();
-		listX.push_front(x);
-		listY.push_front(y);
-	}
+    // Initialize a new bullet at the howitzer's position
+    void newBullet(Position* howitzer)
+    {
+        // Clear any existing values from the deques
+        listX.clear();
+        listY.clear();
 
-	void updatePosition()
-	{
-		x = x + componentX;
+        // Populate deques with zeroes
+        for (int count = 1; count <= 19; count++)
+        {
+            listX.push_front(0);
+            listY.push_front(0);
+        }
 
-		listX.pop_back();
-		listX.push_front(x);
+        // Set the initial position of the bullet to the howitzer's position
+        x = howitzer->getMetersX();
+        y = howitzer->getMetersY();
 
+        // Store the initial position in the deques
+        listX.push_front(x);
+        listY.push_front(y);
+    }
 
-		y = y + componentY;
+    // Update the position of the bullet
+    void updatePosition()
+    {
+        // Update the x-coordinate
+        x = x + componentX;
 
-		listY.pop_back();
-		listY.push_front(y);
+        // Update the deque for x
+        listX.pop_back();
+        listX.push_front(x);
 
-		if (y < 0)
-		{
-			for (int count = 0; count < 20; count++)
-			{
-				listX.pop_back();
-				listX.push_front(0);
-				listY.pop_back();
-				listY.push_front(0);
-			}
-		}
-	}
+        // Update the y-coordinate
+        y = y + componentY;
+
+        // Update the deque for y
+        listY.pop_back();
+        listY.push_front(y);
+
+        // If the y-coordinate is less than zero
+        if (y < 0)
+        {
+            // Clear the deques and replace with zeroes
+            for (int count = 0; count < 20; count++)
+            {
+                listX.pop_back();
+                listX.push_front(0);
+                listY.pop_back();
+                listY.push_front(0);
+            }
+        }
+    }
 
 private:
-	Position position = Position();
+    // Unused Position object
+    Position position = Position();
 };
